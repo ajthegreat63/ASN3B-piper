9f0e6c7a4a1f86559191245fbddf49a09567f835
joe <josephfielding03@gmail.com>
2025-10-08 20:06:19 -0400

    comments for GameState

--- src/GameState.java
+++ src/GameState.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+// Represents a GameState
 public class GameState
 {
-
+    // Number of rows and columns on the board
     private static final int ROWS = 8;
     private static final int COLS = 8;
 
-    private ArrayList<Cell> cells;
-    private boolean blackTurn;
+    private ArrayList<Cell> cells; // cells on the game board
+    private boolean blackTurn;     // flag for current turn
 
+    /**
+     * Creates a new GameState
+     */
     private GameState() {}
 
+    /**
+     * Creates a GameState
+     * @param cells the 64 cells in the board
+     * @param blackTurn true if it is black's turn in the state, false if red's
+     */
     private GameState(ArrayList<Cell> cells,  boolean blackTurn) {
         this.cells = cells;
         this.blackTurn = blackTurn;
     }
 
+    /**
+     * Removes a piece from the game board
+     * @param piece_index the index on the board of the piece to remove
+     * @return a new GameState with the piece at the given index removed
+     */
     private GameState removePiece(int piece_index) {
         GameState newState = new GameState();
 
@@ -30,6 +44,12 @@ public class GameState
         return newState;
     }
 
+    /**
+     * Moves a piece and changes the turn to the other color
+     * @param moved_cell_start_index the initial index of the piece to move
+     * @param moved_cell_end_index the index to move the piece to
+     * @return Updated GameState
+     */
     private GameState movePieceFlipTurn(
         int moved_cell_start_index,
         int moved_cell_end_index
@@ -45,6 +65,11 @@ public class GameState
         return newState;
     }
 
+    /**
+     * Get the row index of a Cell
+     * @param cell The cell represented as [A-H][1-8]
+     * @return the row index (0-7)
+     */
     private int rowIndex(String cell) {
         if (cell.length() < 2) {
             return -1;
@@ -53,6 +78,11 @@ public class GameState
         return cell.charAt(1) - '1';
     }
 
+    /**
+     * Get the col index of a Cell
+     * @param cell The cell represented as [A-H][1-8]
+     * @return the col index (0-7)
+     */
     private int colIndex(String cell) {
         if (cell.length() < 2) {
             return -1;
@@ -61,6 +91,12 @@ public class GameState
         return cell.charAt(0) - 'A';
     }
 
+    /**
+     * Populate the array of next possible moves for a piece
+     * @param start_index The initial index of the piece
+     * @param curr_index The curr index (as we are searching possible moves)
+     * @param nextMoves The array to populate the moves in
+     */
     private void populateNextMoves(
             int start_index,
             int curr_index,
@@ -84,6 +120,11 @@ public class GameState
         }
     }
 
+    /**
+     * Gets all the possible moves of a piece located at a cell
+     * @param cell The cell represented as [A-H][1-8]
+     * @return The list of moves
+     */
     public ArrayList<GameState> nextMovesFromCell(String cell) {
         int r = rowIndex(cell);
         int c = colIndex(cell);
@@ -100,6 +141,10 @@ public class GameState
         return nextGameStates;
     }
 
+    /**
+     * Gets a string representation of the board
+     * @return String of the board
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -120,6 +165,15 @@ public class GameState
         return sb.toString();
     }
 
+    /**
+     * Gets a GameState represented in a file, also given whose turn it is
+     * @param filepath Path of the file containg the board
+     * @param blackTurn true if it is black's turn, false if red's turn
+     * @return the GameState
+     * @throws FileNotFoundException Board file not found
+     * @throws NoSuchElementException Thrown if error parsing board
+     * @throws IllegalArgumentException Thrown if error parsing board
+     */
     public static GameState fromFile(
         String filepath,
         boolean blackTurn
@@ -161,10 +215,18 @@ public class GameState
         return new GameState(cells, blackTurn);
     }
 
+    /**
+     * Gets the current turn
+     * @return true if it is black's turn, false if red's turn
+     */
     public boolean isBlackTurn() {
         return this.blackTurn;
     }
 
+    /**
+     * Gets the board of the GameState
+     * @return List of cells in the board
+     */
     public ArrayList<Cell> getCells() {
         return this.cells;
     }
